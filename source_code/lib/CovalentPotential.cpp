b567bbe71b4c3180db95c13ba34dcf0abcaa9164
ayebex <mariond@student.ethz.ch>
2017-05-09 14:38:26 +0200

    stand 9.5.

---
+++ source_code/lib/CovalentPotential.cpp
@@ -0,0 +1,23 @@
+//
+// Created by mariond on 09.05.17.
+//
+
+#include "CovalentPotential.h"
+
+
+
+CovalentPotential::CovalentPotential(double r, double r0, MDParameters par, bool withangle) : Potential(r, r0), par(par), withangle(withangle){
+
+     bonds = new HarmonicPotential(1, r, r0); //TO DO: implement kr in MDParameters
+     angles = new HarmonicPotential(1, r, r0); // "
+     dihedral = new TorsionPotential(r, r0, 1, 1); //TO DO: implement omega and gamma in MDParameters
+}
+
+double CovalentPotential::computePotential() {
+    return bonds->computePotential() + angles->computePotential() + dihedral->computePotential();
+}
+
+double CovalentPotential::computeForceMagnitude(){
+    return bonds->computeForceMagnitude() + angles->computeForceMagnitude() + dihedral->computeForceMagnitude();
+}
+
---
+++ source_code/lib/CovalentPotential.h
@@ -0,0 +1,34 @@
+//
+// Created by mariond on 09.05.17.
+//
+
+#ifndef MDATOM_COVALENTPOTENTIAL_H
+#define MDATOM_COVALENTPOTENTIAL_H
+
+
+#include "Potential.h"
+#include "HarmonicPotential.h"
+#include "TorsionPotential.h"
+#include "MDParameters.h"
+
+class CovalentPotential : Potential{
+
+public:
+    bool withangle;
+
+    HarmonicPotential *bonds;
+    HarmonicPotential *angles;
+    TorsionPotential *dihedral;
+    MDParameters par;
+
+    CovalentPotential(double r, double r0, MDParameters par, bool withangle);
+
+    double computePotential() override;
+
+    double computeForceMagnitude() override ;
+
+
+};
+
+
+#endif //MDATOM_COVALENTPOTENTIAL_H
